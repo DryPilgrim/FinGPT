3773bb6d434ebd85181c039c24e7cf86ab11eb10
段仁语 <dry01602425@alibaba-inc.com>
2023-04-25 10:49:37 +0800

    sort according age

--- dry.py
+++ dry.py
@@ -3,13 +3,13 @@ import pandas as pd
 def df_sort(df, feature):
     features = [i for i in range(len(df))]
     df[feature] = features
-    return df.sort_values(feature, ascending=True)
+    df['money'] = [i for i in range(len(df))]
+    return df.sort_values('age', ascending=True)
 
 # 创建一个 DataFrame
 df = pd.DataFrame({
     'name': ['Tom', 'Jack', 'Mary'],
-    'age': [20, 25, 18],
-    'grade': [80, 90, 85]
+    'age': [20, 25, 18]
 })
 
 print(df_sort(df, 'ok'))
